f2e355c23d6b8238bb3208a22fc33838664edc02
DonoMollie <donovan.nguyen@mollie.com>
2023-05-24 12:30:14 +0200

    I did so much stuff i don't even know Added .env support added traefik support edited some lines in compose

--- .docker/mollie.sh
+++ .docker/mollie.sh
@@ -8,15 +8,10 @@ then
     wget -q --no-check-certificate --content-disposition https://github.com/mollie/Shopware/releases/download/v2.4.0/MollieShopware-v2.4.0.zip        
     unzip  -q ./MollieShopware-v2.4.0.zip
     
-    cd ../../
-    php bin/console plugin:refresh
-    php bin/console plugin:install MolliePayments
-    php bin/console plugin:activate MolliePayments
-    php bin/console cache:clear
 fi
 
 if [ -f ./MollieShopware-v2.4.0.zip ]
 then rm ./MollieShopware-v2.4.0.zip
 
 fi
-echo "[!] Mollie plugin is installed!"
+echo "[!] Mollie plugin is installed, and .zip file is deleted!"
